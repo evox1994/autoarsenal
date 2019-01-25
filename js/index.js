ef62a10039c9923a0291fbe4a5b158c2009e9571
evox1994 <dmitrybatukhtin@gmail.com>
2019-01-25 13:36:04 +0700

    ывамвам

--- js/index.js
+++ js/index.js
@@ -3,7 +3,21 @@ $(document).ready(function(){
 	$('.b-2-slider').slick({
 		dots: true,
 		arrows: false,
-		slidesToShow: 3
+		slidesToShow: 3,
+		responsive: [
+			{
+				breakpoint: 767,
+				settings: {
+					slidesToShow: 2
+				}
+			},
+			{
+				breakpoint: 500,
+				settings: {
+					slidesToShow: 1
+				}
+			}
+		]
 	});
 
 });
